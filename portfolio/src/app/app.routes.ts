497c481201847d37c2a0c10f24c3cbafe3ac3da5
Renato Presoto <renatopresoto.dev@gmail.com>
2025-10-29 14:17:09 -0300

    Configuração inicial de rotas

--- portfolio/src/app/app.routes.ts
+++ portfolio/src/app/app.routes.ts
@@ -1,3 +1,14 @@
 import { Routes } from '@angular/router';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+    {
+        path:'',
+        redirectTo:'Welcome',
+        pathMatch:'full'
+    },
+    {
+        path:'Welcome',
+        loadComponent: () => import ('./pages/landing-page/landing-page.component').then(page => page.LandingPageComponent),
+        title: 'Renato Presoto Portfolio',
+    },
+];
