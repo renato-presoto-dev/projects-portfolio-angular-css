f40d9bebdc2f270f168d4bd91a93840ebbd92d35
Renato Presoto <renatopresoto.dev@gmail.com>
2026-02-18 16:17:16 -0300

    Funcionalidade de habilidades com logo das stacks

--- portfolio/src/app/pages/landing-page/landing-page.component.ts
+++ portfolio/src/app/pages/landing-page/landing-page.component.ts
@@ -1,9 +1,10 @@
+import { CommonModule } from '@angular/common';
 import { Component, ElementRef, ViewChild } from '@angular/core';
 
 @Component({
   selector: 'app-landing-page',
   standalone: true,
-  imports: [],
+  imports: [CommonModule],
   templateUrl: './landing-page.component.html',
   styleUrl: './landing-page.component.css'
 })
@@ -16,5 +17,27 @@ export class LandingPageComponent {
       })
     }
   }
+
+  itemAtivo: number | null = null;
+
+  // Seus dados (Imagens e Descrições correspondentes)
+  habilidades = [
+    { 
+      img: './icons/angularlogo.png', 
+      alt: 'Angular', 
+      descricao: 'Desenvolvimento de SPAs robustas com Angular.' 
+    },
+    { 
+      img: 'assets/img2.png', 
+      alt: 'CSS', 
+      descricao: 'Estilização avançada e responsiva com CSS3.' 
+    },
+    { 
+      img: 'assets/img3.png', 
+      alt: 'TypeScript', 
+      descricao: 'Tipagem forte e recursos modernos de JS.' 
+    },
+    // Adicione quantos itens precisar...
+  ];
 
 }
